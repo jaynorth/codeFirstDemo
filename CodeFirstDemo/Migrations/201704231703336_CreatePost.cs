38225823527898c637dbdb1136b30e9262f3edf2
jay <jayrdee@gmail.com>
2017-04-23 19:04:45 +0200

    add-migration CreatePost // in package manager

---
+++ CodeFirstDemo/Migrations/201704231703336_CreatePost.cs
@@ -0,0 +1,28 @@
+namespace CodeFirstDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        DatePublished = c.DateTime(nullable: false),
+                        Title = c.String(),
+                        Body = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
